1d1a8f50cee8d88ba94a35105d057c53dafa9476
aalexander3 <aralx73@gmail.com>
2018-02-03 16:36:41 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
-  # write code here
+  attr_reader :side1, :side2, :side3, :sides
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+    @sides = [side1, side2, side3].sort
+  end
+
+  def kind
+    case
+    when self.illegal
+      raise TriangleError
+    when self.side1 == self.side2 && self.side1 == self.side3
+      :equilateral
+    when self.side1 == self.side2 || self.side1 == self.side3 || self.side2 == self.side3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def illegal
+    if self.sides.any? {|side| side == 0}
+      true
+    elsif self.sides[0] + self.sides[1] <= self.sides[2]
+      true
+    else
+      false
+    end
+  end
+
+end
+
+class TriangleError < StandardError
+
 end
